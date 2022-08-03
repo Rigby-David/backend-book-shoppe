ae1ffc4dda6fa212d79856e687d06c170b37e947
David <Rigby335@hotmail.com>
2022-08-03 10:26:19 -0700

    /authors test fails

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,6 +1,5 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const { request } = require('express');
 const request = require('supertest');
 const app = require('../lib/app');
 
@@ -8,9 +7,18 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('#GET should return author id and their name', async () => {
+  it('#GET /books should return book id, title and released', async () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toBe(9);
+    expect(res.body[0]).toEqual({
+      id: '1',
+      title: 'The Hobbit or There and Back Again',
+      released: 1937,
+    });
+  });
+  it('#GET /authors should return id and name', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.body.length).toBe(4);
   });
   afterAll(() => {
     pool.end();
